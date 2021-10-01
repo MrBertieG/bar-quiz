9837fefc29c948e1c5b448ae3d42b00c86c5b95a
Bertie <albertogheorghita90@gmail.com>
2021-10-01 09:03:48 +0000

    adjust layout index page

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,3 +1,8 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+const mainImg = document.querySelector('#main-banner-container');
+const slider = document.querySelector('.slider');
+const mainIntro = document.querySelector('#main-intro');
+const titleLogo = document.querySelector('#title-logo');
