2b56639814b885a3f6fbc7e2cd60d36139de5838
Bertie <albertogheorghita90@gmail.com>
2021-10-02 17:02:55 +0000

    Add pop up window to rules button

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,3 +1,4 @@
+//Hamburger Menu Effect
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
@@ -8,3 +9,36 @@ hamburger.addEventListener('click', () => {
     link.classList.toggle('fade');
   });
 });
+
+//Rules Selector
+const openRulesButtons = document.querySelectorAll('[data-rules-target]');
+const closeRulesButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementsByClassName('rules-overlay');
+
+openRulesButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const rules = document.querySelector(button.dataset.rulesTarget);
+    openRules(rules);
+  });
+});
+
+closeRulesButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const rules = button.closest('.rules');
+    closeRules(rules);
+  });
+});
+
+function openRules(rules) {
+  if (rules == null) 
+  return
+    rules.classList.add('active'),
+    overlay.classList.add('active');
+  };
+
+  function closeRules(rules) {
+    if (rules == null) 
+    return 
+    rules.classList.remove('active'),
+    overlay.classList.remove('active');
+  };
