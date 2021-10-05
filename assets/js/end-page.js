d0212785033208d4b828103c635b2b86e1dc806f
Bertie <albertogheorghita90@gmail.com>
2021-10-05 13:39:53 +0000

    Add javascript functions to enanble the user to save the score at the end of the game

--- assets/js/end-page.js
+++ assets/js/end-page.js
@@ -11,20 +11,24 @@ const max_high_scores = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value 
-})
+    saveScoreBtn.disabled = !username.value ;
+});
+
+//Prevents autorefresh
 
 saveHighScore = e => {
+    e.preventDefault ()
+
     const score = {
         score: mostRecentScore,
         name: username.value
     };
 
-    highScores.push(score);
+    highScores.push(score)
 
     highScores.sort((a,b) => {
         return b.score - a.score
-    };)
+    });
 
     highScores.splice(5);
 
